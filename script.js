01ea315cd3d52d2e3d04b1bb95ef4acc08bc68bc
Malte <wombatconrad@gmail.com>
2022-02-12 19:27:34 +0100

    :(
    fungerar inte o lägga in bilden, den säger att img är null?

--- script.js
+++ script.js
@@ -2,41 +2,51 @@ start();
 
 async function start(){
 
+  let pictureTemplate = document.querySelector('#picture-template');
+  pictureTemplate.remove;
+
   let params = new URLSearchParams({
     key: '25658759-75048e1e43fe34cc1d40a96f8',
   });
 
   let images = await fetch('https://pixabay.com/api/?' + params.toString())
-  .then(response => response.json());
-  //since then is together with fetch... do we need an await before second response?
-  //then returns a promise p.then(onFulfilled[, onRejected]);
-  //we don't need laffy's page stuff bc api??
-  //console.log('images', images);
+  .then(response => response.json())
+  .then (data => {
 
-//images is not iterable?? :(
-//undefined = not yet initialized... but should be?
-  for(let image of images){
-    fetchAllImages(image);
-  }
+    var images = [];
+
+    if(data.hits.length >= 10)
+    {
+      images = data.hits.slice(0,10);
+    }
+
+    for(image of images)
+    {
+      fetchAllImages(image);
+    }
+  });
 
 //===================================
 
   function fetchAllImages(image){
 
-    let pic = document.createElement('img');
-    pic.setAttribute('src', image.previewURL);
+    //let pic = document.createElement('img');
+    //pic.setAttribute('src', image.previewURL);
       //img in createElement = tag name, specifies the type of element created
       //setAttribute adds url into src in html
 
-    let user = document.createElement('span');
-    user.textContent = image.user;
+    //let user = document.createElement('span');
+    //user.textContent = image.user;
 
-    let tags = document.createElement('span');
-    tags.textContent = image.tags;
+    //let tags = document.createElement('span');
+    //tags.textContent = image.tags;
 
-    document.getElementByClassName('pic').appendChild(image);
-    document.getElementByClassName('contributor').appendChild(user);
-    document.getElementByClassName('tags').appendChild(tags);
+    let listItem = pictureTemplate.content.firstElementChild.cloneNode(true);
+    listItem.querySelector('.pic').setAttribute('src', image.webformatURL);
+    listItem.querySelector('.user').textContent = image.user;
+    listItem.querySelector('.tags').textContent = image.tags;
+
+    listItem.append();
 
   }//fetchAllImages
 }//start
